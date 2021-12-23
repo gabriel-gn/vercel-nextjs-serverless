88df610f744bb2114da3cb36af27c8ffbe1b5d4a
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-12-23 17:22:43 -0300

    :sparkles: get deck by code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Param} from '@nestjs/common';
+import {AppService} from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {
+    }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    rootPath() {
+        return `Healthcheck Ok`;
+    }
+
+    @Get('deck/:deckCode')
+    getDeckByCode(@Param('deckCode') deckCode: string): { card: any, count: number }[] {
+        return this.appService.getDeckByCode(deckCode);
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable} from '@nestjs/common';
+import {
+    getCodeFromDeck,
+    getDeckFromCode,
+    CardCodeAndCount,
+    Deck
+} from 'lor-deckcodes-ts';
+import * as set1 from '../src/assets/en_us/set1-en_us.json';
+import * as set2 from '../src/assets/en_us/set2-en_us.json';
+import * as set3 from '../src/assets/en_us/set3-en_us.json';
+import * as set4 from '../src/assets/en_us/set4-en_us.json';
+
+const cards = [...set1, ...set2, ...set3, ...set4];
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getDeckByCode(deckCode: string): { card: any, count: number }[] {
+        const decodedDeck: Deck = getDeckFromCode(deckCode);
+        const finalDeck: { card: any, count: number }[] = decodedDeck.map(card => {
+            return {
+                card: cards.find(lorCard => lorCard.cardCode === card.cardCode),
+                count: card.count
+            }
+        })
+        return finalDeck;
+    }
 }
