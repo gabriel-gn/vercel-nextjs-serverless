734fc37bee9175235d31ce9fe37310ba4f91a970
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-12-23 22:40:50 -0300

    :sparkles: serverless decoded deck?

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,20 +5,23 @@ import {
     CardCodeAndCount,
     Deck
 } from 'lor-deckcodes-ts';
-import * as set1 from '../src/assets/en_us/set1-en_us.json';
-import * as set2 from '../src/assets/en_us/set2-en_us.json';
-import * as set3 from '../src/assets/en_us/set3-en_us.json';
-import * as set4 from '../src/assets/en_us/set4-en_us.json';
-
-const cards = [...set1, ...set2, ...set3, ...set4];
 
 @Injectable()
 export class AppService {
+    private cards;
+    constructor() {
+        const set1 = require('./assets/en_us/set1-en_us.json');
+        const set2 = require('./assets/en_us/set2-en_us.json');
+        const set3 = require('./assets/en_us/set3-en_us.json');
+        const set4 = require('./assets/en_us/set4-en_us.json');
+        this.cards = [...set1, ...set2, ...set3, ...set4];
+    }
+
     getDeckByCode(deckCode: string): { card: any, count: number }[] {
         const decodedDeck: Deck = getDeckFromCode(deckCode);
         const finalDeck: { card: any, count: number }[] = decodedDeck.map(card => {
             return {
-                card: cards.find(lorCard => lorCard.cardCode === card.cardCode),
+                card: this.cards.find(lorCard => lorCard.cardCode === card.cardCode),
                 count: card.count
             }
         })
