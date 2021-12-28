1c3eed127013e775a8406b95916e1e8b40076e7c
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2021-12-27 22:16:07 -0300

    :wrench: remove cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.enableCors();
   await app.listen(3000);
 }
 
