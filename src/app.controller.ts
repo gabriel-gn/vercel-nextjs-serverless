56fb7cbe5878b4fbd6f935b2f370a4e332b306d2
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-01-09 00:09:48 -0300

    :wrench: remoção de imports desnecessários

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,8 @@
-import { Body, Controller, Get } from "@nestjs/common";
-import {AppService} from './app.service';
+import { Controller, Get } from "@nestjs/common";
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {
+    constructor() {
     }
 
     @Get()
