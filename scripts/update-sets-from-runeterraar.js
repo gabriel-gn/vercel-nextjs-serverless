b145259a883e8ee704cad4941c31acb72334c1b6
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-02-13 16:42:00 -0300

    :wrench: removendo script desnecessário

--- scripts/update-sets-from-runeterraar.js
+++
@@ -1,8 +0,0 @@
-const needle = require('needle');
-const fs = require('fs');
-
-const path = 'https://runeterra.ar/cards/builder/get/en_us?take=1000000'
-
-needle.post(path, {}, { json: true }, function (error, response) {
-  fs.writeFileSync('src/sets/runeterraAR/en_us.json', JSON.stringify(response.body.cards))
-});
