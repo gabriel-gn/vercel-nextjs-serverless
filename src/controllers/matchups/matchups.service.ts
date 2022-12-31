0c6640c4e72e845b6ee30838fcc00ff42d952622
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-12-31 16:48:53 -0300

    :wrench: mais um teste de deploy

--- src/controllers/matchups/matchups.service.ts
+++ src/controllers/matchups/matchups.service.ts
@@ -62,7 +62,7 @@ export class MatchupsService {
 
   public get(): Observable<any> {
     return this.getCsvContent(
-      '../assets/matchups_data/latest/latest_diamond.csv',
+      '../../../assets/matchups_data/latest/latest_diamond.csv',
     );
   }
 
