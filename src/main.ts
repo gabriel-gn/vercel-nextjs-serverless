3adc4793a6eea9bfe0c9c135064bd1b83c376587
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2022-12-31 18:18:41 -0300

    :wrench: mais uma tentativa vercel...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from "@nestjs/core";
-import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from "@nestjs/swagger";
-import { AppModule } from "./app.module";
-import { RedocModule, RedocOptions } from "nestjs-redoc";
-import { LanguageInterceptor } from "./shared/interceptors/language.interceptor";
+import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { RedocModule, RedocOptions } from 'nestjs-redoc';
+import { LanguageInterceptor } from './shared/interceptors/language.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
@@ -11,28 +15,38 @@ async function bootstrap() {
   if (process.env.VERCEL_ENV !== 'production') {
     const swaggerConfig = new DocumentBuilder()
       .setTitle('Legends of Runeterra Decks')
-      .setDescription('Get Decks from mobalytics, Runeterra AR and card information for Legends of Runeterra card game.')
+      .setDescription(
+        'Get Decks from mobalytics, Runeterra AR and card information for Legends of Runeterra card game.',
+      )
       .setVersion('')
-      .addTag('Factions', '<SchemaDefinition schemaRef=\"#/components/schemas/Factions\" />')
-      .addTag('SearchDeckLibraryDto', '<SchemaDefinition schemaRef=\"#/components/schemas/SearchDeckLibraryDto\" />')
+      .addTag(
+        'Factions',
+        '<SchemaDefinition schemaRef="#/components/schemas/Factions" />',
+      )
+      .addTag(
+        'SearchDeckLibraryDto',
+        '<SchemaDefinition schemaRef="#/components/schemas/SearchDeckLibraryDto" />',
+      )
       // .addTag('cats')
-      .build()
-    ;
-    const swaggerOptions: SwaggerDocumentOptions =  {
-      operationIdFactory: (
-        controllerKey: string,
-        methodKey: string
-      ) => methodKey
+      .build();
+    const swaggerOptions: SwaggerDocumentOptions = {
+      operationIdFactory: (controllerKey: string, methodKey: string) =>
+        methodKey,
     };
-    const document = SwaggerModule.createDocument(app, swaggerConfig, swaggerOptions);
+    const document = SwaggerModule.createDocument(
+      app,
+      swaggerConfig,
+      swaggerOptions,
+    );
 
     const redocOptions: RedocOptions = {
       title: 'Legends of Runeterra Decks API Documentation',
-      favicon: 'https://lor.cardsrealm.com/ezoimgfmt/cdn.cardsrealm.com/images/icon/lor.png?width=45&ezimgfmt=rs:0x0/rscb1/ng:webp/ngcb1',
+      favicon:
+        'https://lor.cardsrealm.com/ezoimgfmt/cdn.cardsrealm.com/images/icon/lor.png?width=45&ezimgfmt=rs:0x0/rscb1/ng:webp/ngcb1',
       logo: {
         url: 'https://upload.wikimedia.org/wikipedia/it/thumb/1/17/Logo-No_Tag_No-Faction-ENG-RGB.png/260px-Logo-No_Tag_No-Faction-ENG-RGB.png',
         // backgroundColor: '#F0F0F0',
-        altText: 'Runeterra Logo'
+        altText: 'Runeterra Logo',
       },
       requiredPropsFirst: true,
       sortPropsAlphabetically: true,
@@ -41,7 +55,7 @@ async function bootstrap() {
       auth: {
         enabled: false,
         user: 'admin',
-        password: '123'
+        password: '123',
       },
       tagGroups: [
         {
@@ -50,10 +64,7 @@ async function bootstrap() {
         },
         {
           name: 'Models',
-          tags: [
-            'Factions',
-            'SearchDeckLibraryDto'
-          ],
+          tags: ['Factions', 'SearchDeckLibraryDto'],
         },
       ],
     };
