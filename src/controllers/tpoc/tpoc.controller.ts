8f3cfec6bd9d3dd8d7f3af41822a9d8291cce5db
Gabriel Nogueira <gabrielgomesnogueira@gmail.com>
2024-02-14 17:26:42 -0300

    :wrench: endpoint API for POC

--- src/controllers/tpoc/tpoc.controller.ts
+++ src/controllers/tpoc/tpoc.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { TpocService } from './tpoc.service';
+import {Controller, Get, Query} from '@nestjs/common';
+import {ApiTags} from '@nestjs/swagger';
+import {TpocService} from './tpoc.service';
 import {RiotAssetsService} from "../riot-assets/riot-assets.service";
+import {ApiImplicitQuery} from "@nestjs/swagger/dist/decorators/api-implicit-query.decorator";
+import {from, of} from "rxjs";
 
 @ApiTags('Path of Champions')
 @Controller()
@@ -9,7 +11,8 @@ export class TpocController {
   constructor(
     private readonly tpocService: TpocService,
     private readonly riotAssetsService: RiotAssetsService
-  ) {}
+  ) {
+  }
 
   @Get()
   async get() {
@@ -22,7 +25,18 @@ export class TpocController {
   }
 
   @Get('champion-builds')
-  async getOptimalTpocChampionBuilds() {
-    return this.tpocService.getOptimalTpocChampionBuilds();
+  @ApiImplicitQuery({
+    name: 'raw',
+    description: 'if true, use the google sheet to get real time state of updates',
+    required: false,
+    type: Boolean,
+  })
+  async getOptimalTpocChampionBuilds(@Query('raw') raw: boolean = false) {
+    raw = `${raw}`.toLowerCase() === 'true';
+    if (raw) {
+      return this.tpocService.getOptimalTpocChampionBuilds();
+    } else {
+      return of(require(`../../assets/tpoc/static/champion_builds.json`));
+    }
   }
 }
